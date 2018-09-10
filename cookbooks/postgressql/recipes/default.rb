cede24c211738bff9504f0d510496a8bf84ff19c
root <root@workstation.localdomain.net>
2018-09-10 07:50:51 -0400

    add postgress

---
+++ cookbooks/postgressql/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: postgressql
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'postgresql-server' do
+notifies :run, 'execute[postgresql-init]', :immediate
+end
+
+execute 'postgresql-init' do
+command 'postgresql-setup initdb'
+action :nothing
+end
+
+service 'postgresql' do
+action [:enable, :start]
+end
